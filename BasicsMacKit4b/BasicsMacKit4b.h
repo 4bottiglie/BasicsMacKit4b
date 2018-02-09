32a1a7b6a209201030340215c2fb1b42ff98c47f
Sorin George Budescu <4bottiglie@gmail.com>
2018-02-09 21:42:33 +0100

    Initial Commit

---
+++ BasicsMacKit4b/BasicsMacKit4b.h
@@ -0,0 +1,19 @@
+//
+//  BasicsMacKit4b.h
+//  BasicsMacKit4b
+//
+//  Created by Sorin George Budescu on 09/02/18.
+//  Copyright © 2018 Sorin George Budescu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BasicsMacKit4b.
+FOUNDATION_EXPORT double BasicsMacKit4bVersionNumber;
+
+//! Project version string for BasicsMacKit4b.
+FOUNDATION_EXPORT const unsigned char BasicsMacKit4bVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicsMacKit4b/PublicHeader.h>
+
+
